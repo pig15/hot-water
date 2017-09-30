d80034c227bf9cb96a372e9362cc314b40fc82df
lixiaotian17 <31760802+lixiaotian17@users.noreply.github.com>
2017-09-30 11:16:48 +0800

    Create Activity.java
    实体类

---
+++ Activity.java
@@ -0,0 +1,18 @@
+package com.gosu.piggymate.beans;
+
+
+public class Activity implements java.io.Serializable {
+    //活动id
+    public String id;
+    //活动名称
+    public String name;
+    //活动详情
+    public String content;
+    //活动配置
+    public String configs;
+    //创建时间
+    public java.util.Date createdAt;
+    //更新时间
+    public java.util.Date updatedAt;
+
+}
